e238b24a4017b4829566d3da921f1e49c3f01017
Brahim azreg <brahimaz@hotmail.com>
2020-02-05 14:50:54 +0000

    reverse factorial

--- src/com/qa/factorial/MyFactorial.java
+++ src/com/qa/factorial/MyFactorial.java
@@ -2,17 +2,26 @@ package com.qa.factorial;
 
 public class MyFactorial {
 
-	public static void main(String[] args) {
-        int number=4;
-		int fact = 1;
-		for( int i = number;i >1;i--) {
-			fact = fact* (i);
-		}
-		
-		System.out.println(fact);
-
-	}
-  
-	
-	
+	 static String  temp="";
+		public static String factorial(int n){ 
+	 
+		 
+		 
+		  if (n == 0)    { return "1";   } 
+		    
+			  for (int  i = n; i >1; i--) {
+				   temp=temp+ String.valueOf(i)+"/";
+				   
+			  }
+			  return temp;  
+		 }    
+		  
+		  
+		 public static void main(String args[]){  
+		  //int i,fact=1;  
+		//  int number=4;//It is the number to calculate factorial    
+		  //fact = factorial(20);   
+		  System.out.println( factorial(5));    
+		 }  
+		 
 }
