7cc0980d54873190e9647ce779ba062fac609cee
ccgr1121 <48327619+ccgr1121@users.noreply.github.com>
2020-02-05 14:16:01 +0000

    Add files via upload

---
+++ src/com/qa/factorial/BrahimFactorial.java
@@ -0,0 +1,16 @@
+package com.qa.factorial;
+
+public class BrahimFactorial {
+
+	public static void main(String[] args) {
+		int number = 4;
+		int fact = 1;
+		for (int i = number; i > 1; i--) {
+			fact = fact * (i);
+		}
+
+		System.out.println(fact);
+
+	}
+
+}
---
+++ src/com/qa/factorial/Runner.java
@@ -0,0 +1,23 @@
+package com.qa.factorial;
+
+public class Runner {
+
+	public static void main(String[] args) {
+
+//		if (n == 0)    
+//		    return 1;    
+//		  else    
+//		    return(n * factorial(n-1));    
+//		 }    
+//		 public static void main(String args[]){  
+//		  int i,fact=1;  
+//		  int number=4;//It is the number to calculate factorial    
+//		  fact = factorial(number);   
+//		  System.out.println("Factorial of "+number+" is: "+fact);    
+//		 }  
+//		}  
+//		
+
+
+	}
+}
